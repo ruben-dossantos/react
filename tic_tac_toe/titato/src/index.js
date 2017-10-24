099f9cc0867555a6032b98ec9314137cb6b45ca4
Ruben <xumbino@gmail.com>
2017-10-24 14:10:46 +0100

    history added

--- tic_tac_toe/titato/src/index.js
+++ tic_tac_toe/titato/src/index.js
@@ -6,8 +6,8 @@ import './index.css'
 class Square extends React.Component {
 	render(){
 		return (
-			<button 
-				className="square" 
+			<button
+				className="square"
 				onClick={ () => this.props.onClick() }>
 				{this.props.value}
 			</button>
@@ -26,9 +26,9 @@ function Square(props){
 class Board extends React.Component {
 	renderSquare(i) {
 		return (
-			<Square 
+			<Square
 				value={this.props.squares[i]}
-				onClick={() => this.props.onClick(i)} 
+				onClick={() => this.props.onClick(i)}
 			/>
 		)
 	}
@@ -61,7 +61,10 @@ class Game extends React.Component {
 		super()
 		this.state = {
 			history: [
-				{squares: Array(9).fill(null)}
+				{ squares: Array(9).fill(null) }
+			],
+			locations: [
+				{ moves: [] }
 			],
 			stepNumber: 0,
 			xIsNext: true
@@ -70,16 +73,20 @@ class Game extends React.Component {
 
 	handleClick(i){
 		const history = this.state.history.slice(0, this.state.stepNumber + 1)
+		const locations = this.state.locations.slice(0, this.state.stepNumber + 1)
 		const current = history[history.length - 1]
 		const squares = current.squares.slice()
 		if(calculateWinner(squares) || squares[i]){
 			return;
 		}
 		squares[i] = this.state.xIsNext ? 'X' : 'O'
+		const present = history.concat([{squares:squares}])
+		let moves = makeLocations(present, 1)
 		this.setState({
-			history: history.concat([{
-				squares: squares
-			}]),
+			history: present,
+			locations: locations.concat({
+				moves: moves
+			}),
 			stepNumber: history.length,
 			xIsNext: !this.state.xIsNext
 		})
@@ -94,12 +101,13 @@ class Game extends React.Component {
 
 	render() {
 		const history = this.state.history
+		const locations = this.state.locations
 		const current = history[this.state.stepNumber]
 		const winner = calculateWinner(current.squares)
 
 		const moves = history.map((step, move) => {
 			const desc = move ?
-				'Go to move #' + move : 
+				'Go to move #' + move + '(' + locations[move].moves + ')':
 				'Go to game start'
 			return (
 				<li key={move}>
@@ -117,7 +125,7 @@ class Game extends React.Component {
 		return (
 			<div className="game">
 				<div className="game-board">
-					<Board 
+					<Board
 						squares={current.squares}
 						onClick={(i) => this.handleClick(i)}
 					/>
@@ -154,4 +162,20 @@ function calculateWinner(squares) {
 		}
 	}
 	return null
+}
+
+function makeLocations(history, index, moves) {
+	moves = moves || []
+	if(history.length === index){
+		return moves
+	}
+	moves.push(addMove(history[index].squares.slice(), moves))
+	return makeLocations(history, index + 1, moves)
+}
+
+function addMove(squares, moves) {
+	for(let i=0; i<squares.length; i++){
+		if(squares[i] && !moves.includes(i)) return i
+	}
+	return
 }
