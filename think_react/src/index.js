d31f954f9aece11ea0564bbdf71f0747a5579899
Ruben <xumbino@gmail.com>
2017-10-27 18:39:36 +0100

    think react working

--- think_react/src/index.js
+++ think_react/src/index.js
@@ -11,12 +11,15 @@ const data = [
 ]
 
 class ProductRow extends React.Component {
-
 	render(){
+    const product = this.props.product
+    const name = product.stocked ?
+      product.name :
+      <span style={{color: 'red'}}> {product.name} </span>
 		return (
 			<tr>
-				<td>{this.props.product.name}</td>
-				<td>{this.props.product.price}</td>
+				<td>{name}</td>
+				<td>{product.price}</td>
 			</tr>
 		)
 	}
@@ -34,21 +37,26 @@ class ProductCategory extends React.Component{
 
 class ProductTable extends React.Component{
 	getCategories(products, index, categories) {
+    categories = categories || []
 		if(products.length === index) return categories
-		categories = categories || []
 		if(!categories.includes(products[index].category)){
 			categories.push(products[index].category)
-		} 
+		}
 		return this.getCategories(products, (index + 1), categories)
 	}
 
 	render(){
-		const categories = this.getCategories(this.props.products, 0)
+    const products = this.props.products
+    const query = this.props.query
+    const filteredProducts = products.filter(product => !query.inStock || ( query.inStock && product.stocked ))
+      .filter(product => product.name.toLowerCase().includes(query.name.toLowerCase()))
+
+		const categories = this.getCategories(filteredProducts, 0)
 		const rows = []
 
 		categories.forEach( category => {
 			rows.push(<ProductCategory category={category} key={category} />)
-			this.props.products.forEach(product => {
+			filteredProducts.forEach(product => {
 				if(product.category === category) rows.push(<ProductRow product={product} key={product.name} />)
 			})
 		})
@@ -69,8 +77,71 @@ class ProductTable extends React.Component{
 	}
 }
 
+class QueryProduct extends React.Component {
+  handleChange(propertyName, event){
+    this.props.handleChange(propertyName, event)
+  }
+
+  render(){
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={this.props.query.name}
+          onChange={this.handleChange.bind(this, 'name')} />
+
+        <br/>
+        <label>
+          <input
+            name="inStock"
+            type="checkbox"
+            checked={this.props.query.inStock}
+            onChange={this.handleChange.bind(this, 'inStock')} />
+            Only show products in stock
+        </label>
+      </form>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      products: this.props.products,
+      query: {
+        name: '',
+        inStock: false
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(propertyName, event) {
+    const query = this.state.query
+    const target = event.target
+    let value = propertyName === 'inStock' ? target.checked : target.value
+    query[propertyName] = value
+    this.setState({query: query})
+  }
+
+
+  render(){
+    return (
+      <div>
+        <QueryProduct
+          handleChange={this.handleChange}
+          query={this.state.query} />
+        <ProductTable
+          query={this.state.query}
+          products={this.state.products} />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-	<ProductTable 
-		products={data} />,
+	<App products={data}/>,
 	document.getElementById('root')
 )
